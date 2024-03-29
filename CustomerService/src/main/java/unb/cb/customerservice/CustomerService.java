96e2d3574f2b15aaf65c8255cf3e2d9dea850b9d
NotLuvii <97616336+NotLuvii@users.noreply.github.com>
2024-03-29 20:56:19 -0300

    more customer

--- CustomerService/src/main/java/unb/cb/customerservice/CustomerService.java
+++ CustomerService/src/main/java/unb/cb/customerservice/CustomerService.java
@@ -1,6 +1,12 @@
 package unb.cb.customerservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class CustomerService implements Customer {
@@ -10,31 +16,34 @@ public class CustomerService implements Customer {
 
     @Override
     public void addCustomer(String username, String password) {
+        String url = "http://localhost:8081/add_user";
+        Map<String, String> params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", password);
+        params.put("type", "customer");
+
+        ResponseEntity<String> response = restTemplate.postForEntity(url, null, String.class, params);
 
     }
 
     @Override
     public void deleteCustomer(String username, String password) {
-        user_DB.delete_user(username, password);
+
     }
 
     @Override
     public void addToOrder(String item) {
-        AddToOrderCommand addToOrderCommand = new AddToOrderCommand();
-        addToOrderCommand.setItem(item);
-        addToOrderCommand.execute();
+
     }
 
     @Override
     public void cancelOrder() {
-        CancelOrderCommand cancelOrderCommand = new CancelOrderCommand();
-        cancelOrderCommand.execute();
+
     }
 
     @Override
     public void processPayment(float amount, int cardNumber, int amountInCard) {
-        Payment payment = new Payment(amount, cardNumber, amountInCard);
-        payment.proccess();
+
     }
 
 
