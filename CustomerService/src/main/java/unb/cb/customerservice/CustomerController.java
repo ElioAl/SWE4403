28b4d04fb9ea051d8b19b23482ad91e602eb76c7
NotLuvii <97616336+NotLuvii@users.noreply.github.com>
2024-03-31 14:30:55 -0300

    customer

--- CustomerService/src/main/java/unb/cb/customerservice/CustomerController.java
+++ CustomerService/src/main/java/unb/cb/customerservice/CustomerController.java
@@ -18,18 +18,18 @@ public class CustomerController {
         return "Customer deleted successfully";
     }
 
-    public String addToOrder(String item) {
-        customerService.addToOrder(item);
-        return "Item added to order successfully";
-    }
-
-    public String cancelOrder() {
-        customerService.cancelOrder();
-        return "Order cancelled successfully";
-    }
-
-    public String processPayment(float amount, int cardNumber, int amountInCard) {
-        customerService.processPayment(amount, cardNumber, amountInCard);
-        return "Payment processed successfully";
-    }
+//    public String addToOrder(String item) {
+//        customerService.addToOrder(item);
+//        return "Item added to order successfully";
+//    }
+//
+//    public String cancelOrder() {
+//        customerService.cancelOrder();
+//        return "Order cancelled successfully";
+//    }
+//
+//    public String processPayment(float amount, int cardNumber, int amountInCard) {
+//        customerService.processPayment(amount, cardNumber, amountInCard);
+//        return "Payment processed successfully";
+//    }
 }
